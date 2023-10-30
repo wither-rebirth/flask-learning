5fd71aadc95dd94800028a5fa644721ab07c362b
wither <xiaoqian1942@outlook.com>
2023-10-30 21:59:41 +0800

    endpoint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 def create_app(test_config=None):
     #create and configure the app
-    app = Flask(__name__, isinstance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY = 'dev',
         DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
@@ -16,3 +16,24 @@ def create_app(test_config=None):
         app.config.from_mapping(test_config)
     
     #ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world'
+    
+    from . import db
+    db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    return app
